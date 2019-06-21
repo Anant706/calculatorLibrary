9656e274e6d41ce1e5c2148578c2c547bbd27ba2
ayadav2 <anant_yadav@eogresources.com>
2019-06-21 17:22:23 -0500

    added multiplication and division module

--- test-reports/test_calculator.py
+++ test-reports/test_calculator.py
@@ -11,5 +11,9 @@ class test_calculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
     def test_multiplication(self):
         assert 4 == calculator.multiply(2, 2)
+
+    def test_division(self):
+        assert 4 == calculator.divide(8, 2)
